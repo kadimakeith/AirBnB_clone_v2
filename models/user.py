3a824791922eff96e8e03210964e3028638f10c2
kadimakeith <keithkadima@gmail.com>
2023-03-22 14:10:19 +0300

    update user model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """User module for the HBNB Project."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
@@ -11,6 +10,7 @@ class User(BaseModel, Base):
     """
     Represents a user for a MySQL database.
     Inherits from SQLAlchemy Base and links to the MySQL table users.
+
     Attributes:
         __tablename__ (str): The name of the MySQL table to store Users.
         email (sqlalchemy String): The user's email address.
@@ -21,12 +21,14 @@ class User(BaseModel, Base):
         reviews (sqlalchemy relationship): The User-Review relationship.
     """
     __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
     places = relationship("Place", backref="user", cascade="delete")
     reviews = relationship("Review", backref="user", cascade="delete")
+
 
         
 
