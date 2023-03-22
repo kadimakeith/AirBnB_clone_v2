55be756e7693878f72872590aed68b2fbc64c313
kadimakeith <keithkadima@gmail.com>
2023-03-22 22:39:14 +0300

    update city model

--- models/city.py
+++ models/city.py
@@ -13,20 +13,8 @@ class City(BaseModel, Base):
     This class defines a City object that inherits from BaseModel and Base.
     It corresponds to the "cities" table in the MySQL database.
     """
-    __tablename__ = "cities"
-
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes a new City object with the given parameters.
-        """
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        """
-        Returns a string representation of the City object.
-        """
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
