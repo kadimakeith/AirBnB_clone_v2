c081d78e2a995637b590c53236db5204960d3b66
kadimakeith <keithkadima@gmail.com>
2023-03-22 14:06:31 +0300

    update city model

--- models/city.py
+++ models/city.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""City model for HBNB Project"""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
+"""
+This module contains the City class for the HBNB project.
+"""
+
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Represents a city for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table cities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        name (sqlalchemy String): The name of the City.
-        state_id (sqlalchemy String): The state id of the City.
+    """
+    This class defines a City object that inherits from BaseModel and Base.
+    It corresponds to the "cities" table in the MySQL database.
     """
     __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     places = relationship("Place", backref="cities", cascade="delete")
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new City object with the given parameters.
+        """
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        """
+        Returns a string representation of the City object.
+        """
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
