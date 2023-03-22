af74f37b84eca139b7f5f09c6998f71e4980e67e
kadimakeith <keithkadima@gmail.com>
2023-03-22 22:17:19 +0300

    update models - review | state | user

--- models/state.py
+++ models/state.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-"""State module for HBNB Project."""
+""" State Module for HBNB project """
+import os
 import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
 from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
+    '''
+        Implementation for the State.
+    '''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="delete")
+    else:
+        name = ""
 
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
-
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
+            """
+                Getter method that returns list of city
+                instances with state_id == State.id
+                FileStorage Relationship between State and City
+            """
+            cities_dict = models.storage.all(City)
             city_list = []
-            for city in list(models.storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
+            for key, value in cities_dict.items():
+                if value.state_id == self.id:
+                    city_list.append(value)
             return city_list
